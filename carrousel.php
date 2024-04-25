72cb9a52d146050500064a25aa18723ec034ceba
annabellecard3nas <119866562+annabellecard3nas@users.noreply.github.com>
2024-04-25 15:32:17 -0400

    carrousel radio bouton

--- carrousel.php
+++ carrousel.php
@@ -9,22 +9,28 @@
 
 function eddym_enqueue(){
 
-$version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
-$version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");    
+   $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
+   $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");    
 
-wp_enqueue_style(   'em_plugin_carrousel_css',
-plugin_dir_url(__FILE__) . "style.css",
-array(),
-$version_css);
+   wp_enqueue_style(   'em_plugin_carrousel_css',
+   plugin_dir_url(__FILE__) . "style.css",
+   array(),
+   $version_css);
 
-wp_enqueue_script(  'em_plugin_carrousel_js',
-plugin_dir_url(__FILE__) ."js/carrousel.js",
-array(),
-$version_js,
-true); //true permet d'ajouter le script a la fin du document 
+   wp_enqueue_script(  'em_plugin_carrousel_js',
+   plugin_dir_url(__FILE__) ."js/carrousel.js",
+   array(),
+   $version_js,
+   true); //true permet d'ajouter le script a la fin du document 
 }
 
 add_action('wp_enqueue_scripts', 'eddym_enqueue');
+/** IMPORTANT
+ * wp_header() juste avant la fermeture la balise fermeture </head>
+ * 
+ * dans footer php
+ * wp_footer() juste avant la balise de fermeture </body>
+ */
 
  function genere_html(){
     /////////////////////////////////////// HTML
@@ -35,9 +41,11 @@ add_action('wp_enqueue_scripts', 'eddym_enqueue');
        <button class="carrousel__x">X</button>
        <figure class="carrousel__figure"></figure>
        <form class="carrousel__form"></form>
+       
        </div>';
        return $contenu;
    
     return $contenu;
    }
+
 add_shortcode('carrousel',"genere_html");
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,70 @@
-(function(){
-    console.log('debut du carrousel')
+(function () {
+    console.log('debut carrousel')
+    let carrousel = document.querySelector(".carrousel");
+    let bouton = document.querySelector(".bouton__ouvrir");
+    let carrousel__x = document.querySelector(".carrousel__x");
+
+    let galerie = document.querySelector('.galerie');
+    // let galerie_img = galerie.querySelector('img');
+
+    
+    let carrousel__figure = document.querySelector(".carrousel__figure");
+    let galerie_img = galerie.querySelectorAll('img');//collection de l'image de la galerie
+
+    let index= 0;
+    for (const elm of galerie_img){
+        creer_image_carrousel(index,elm);
+        index+=1;
+        creer_radio_carrousel(index-1);
+    }
+    function creer_image_carrousel(index,elm){
+        
+        let carrousel__img = document.createElement('img');//premiere image seulement 
+        carrousel__img.dataset.index=index;
+        carrousel__img.classList.add('carrousel__img');
+        carrousel__img.src = elm.src;
+        carrousel__figure.appendChild(carrousel__img);
+        // console.log(carrousel_img.src);
+
+    }
+
+    function creer_radio_carrousel(index){
+        let carrousel__form =document.querySelector('.carrousel__form');
+        let carrousel_radio = document.createElement('input');
+
+        carrousel_radio.classList.add('carrousel_radio')
+
+        carrousel_radio.dataset.index = index;
+
+        carrousel_radio.type='radio';
+
+        carrousel_radio.name="imgRadio";
+
+        carrousel__form.appendChild(carrousel_radio);
+        //creer input
+        //modifier type = radio
+        //name
+        //index
+        //ajouter le radio bouton au formulaire
+        //ecouteur del'evenement change
+
+        carrousel_radio.addEventListener('click',function(){
+            let index = this.dataset.index;
+            let  carrousel__img = carrousel__figure.children;
+            for(const img of carrousel__img){
+                img.style.opacity = 0;
+            }
+            carrousel__img[index].style.opacity=1;
+        })
+
+    }
+
+    bouton.addEventListener("mousedown", function () {
+        carrousel.classList.add('carrousel--ouvrir');
+    })
+
+    carrousel__x.addEventListener("mousedown", function () {
+        carrousel.classList.remove('carrousel--ouvrir');
+    })
+
 })()
